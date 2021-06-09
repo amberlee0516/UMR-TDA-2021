8577d7e0766c623c4f683a88aabc962d3d978cef
astockdill22 <astockdill22@my.whitworth.edu>
2021-06-09 15:21:38 -0500

    Merge branch 'main' of https://github.com/Amber-Patricia-Lee/UMR-TDA-2021

--- pools EDA/AS/pool4upper.R
+++ pools EDA/AS/pool4upper.R
@@ -9,8 +9,107 @@ library(ggplot2)
 library(lubridate)
 
 # Look at the continuous variables
+setwd("/Users/alainastockdill/UMR-TDA-2021/pools EDA/AS")
 
 # Upload the data
-veg4U <- read.csv(file = "ltrm_vegsrs_data_lat_long.csv")
+veg <- read.csv(file = "../2019 pool data/ltrm_vegsrs_data_lat_long.csv")
+water <- read.csv(file = "../2019 pool data/ltrm_water_data_lat_long.csv")
+
+# Split the veg and water data into pool 4
+veg4 <- veg %>% 
+  filter(POOL == "04")
+
+water4 <- water %>%
+  filter(FLDNUM == "1")
+
+# Get just the year
+water4$DATE <- as.Date(water4$DATE, "%m/%d/%Y")
+water4$year <- year(water4$DATE)
+
+# Split into the upper data
+veg4U <- veg4 %>%
+  filter(MSTRATUM == "BWC-U")
+
+water4U <- water4 %>% 
+  filter(LONGITUDE > -92.035)
+
+# Create a set that is just the continuous variables
+continuous <- c('TN','TP','TEMP','DO','TURB','COND','VEL','SS','WDP','CHLcal','SECCHI')
+water4_cont <- water4[continuous]
+
+# Show the split of upper pool 4 on the entire pool data
+ggplot(data = water4, mapping = aes(x = LATITUDE, y = LONGITUDE )) + 
+  geom_point() + 
+  geom_hline(yintercept = -92.035, color = "red")
+
+# Show the split of upper pool 4 zoomed in
+water4U_test = water4 %>% 
+  filter(LONGITUDE > -92.05)
+
+ggplot(data = water4U_test, mapping = aes(x = LATITUDE, y = LONGITUDE )) + 
+  geom_point() +
+  geom_hline(yintercept = -92.035, color = "red")
 
 
+# Different upper pool 4 coloring
+water4U %>% 
+  filter(!is.na(SS)) %>%
+ggplot(, mapping = aes(x = LATITUDE, y = LONGITUDE )) + 
+  geom_point(mapping = aes(color = SS)) +
+  facet_wrap(~year, nrow = 4)
+
+
+# BOX PLOTS
+
+water4U %>%
+  filter(TURB < 100) %>%
+ggplot(, mapping = aes(y = TURB, x = year, group = year)) + 
+  geom_boxplot()
+
+
+water4U %>%
+  filter( SS < 300) %>%
+  ggplot(, mapping = aes(y = SS, x = year, group = year)) + 
+  geom_boxplot()
+
+  ggplot(water4U, mapping = aes(y = SS, x = year, group = year)) + 
+  geom_boxplot()
+
+
+ggplot(data = wzter4, mapping = aes(x = SECCI)) +
+  geom_histogram()
+
+
+ggplot(data = water4, mapping = aes(x = SECCHI, y = TURB )) + 
+  geom_point(mapping = aes(color = TP), alpha = .1)
+ 
+unique(water4$TURB)
+
+
+
+ggplot(data = water4) + 
+  geom_boxplot(mapping = aes(x = TURB))
+
+
+# TN is least change - little variability
+# TEMP inconsistent
+# TO variable between time of day
+
+
+unique(water4U$STRATUM)
+
+ggplot(data = water4U, mapping = aes(x = year, y = TURB)) + 
+  geom_point() +
+  facet_wrap(~STRATUM, nrow = 3, scale = free)
+
+
+# AMBERS CODE
+
+
+
+
+
+
+
+
+  
